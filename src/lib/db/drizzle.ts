6dc777bb2c5c70d77eeea58be4d2ca4f0a9f5be5
aeither <36173828+aeither@users.noreply.github.com>
2025-03-23 16:48:20 +0100

    fix

--- src/lib/db/drizzle.ts
+++ src/lib/db/drizzle.ts
@@ -1,7 +1,8 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import pkg from 'pg';
 import * as schema from "./schema";
+const { Pool } = pkg;
 
 config({ path: ".env" }); // or .env.local
 
